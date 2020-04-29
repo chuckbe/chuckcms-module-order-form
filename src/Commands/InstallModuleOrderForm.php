67e46e8d50487e5500813f853d9314df6af0cf8b
KarelBrijs <karel@chuck.be>
2020-04-29 22:56:56 +0200

    update module version

--- src/Commands/InstallModuleOrderForm.php
+++ src/Commands/InstallModuleOrderForm.php
@@ -52,7 +52,7 @@ class InstallModuleOrderForm extends Command
         $hintpath = 'chuckcms-module-order-form';
         $path = 'chuckbe/chuckcms-module-order-form';
         $type = 'module';
-        $version = '0.1.10';
+        $version = '0.1.11';
         $author = 'Karel Brijs (karel@chuck.be)';
 
         $json = [];
