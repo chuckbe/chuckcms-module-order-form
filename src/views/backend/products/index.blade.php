7e76b42ba4dd04b0dffebd2d0f9f4a2b87d6e487
rawaldeep <rawaldeep@chuck.be>
2020-11-10 11:52:53 +0000

    added new layout to products index

--- src/views/backend/products/index.blade.php
+++ src/views/backend/products/index.blade.php
@@ -1,4 +1,4 @@
-@extends('chuckcms::backend.layouts.admin')
+@extends('chuckcms::backend.layouts.base')
 
 @section('title')
 	Producten
@@ -8,6 +8,66 @@
 	@can('create forms')
 	<a href="{{ route('dashboard.module.order_form.products.create') }}" class="btn btn-link text-primary m-l-20 hidden-md-down">Voeg Nieuw Product Toe</a>
 	@endcan
+@endsection
+
+@section('content')
+<div class="container p-3">
+	<div class="row">
+		<div class="col-sm-12">
+			<nav aria-label="breadcrumb">
+                <ol class="breadcrumb mt-3">
+                    <li class="breadcrumb-item active" aria-current="product">Producten</li>
+                </ol>
+            </nav>
+		</div>
+	</div>
+	<div class="row bg-light shadow-sm rounded p-3 mb-3 mx-1">
+		@can('create forms')
+		<div class="col-sm-12 text-right">
+    		<a href="{{ route('dashboard.module.order_form.products.create') }}" class="btn btn-sm btn-outline-success"> Voeg Nieuw Product Toe </a>
+    	</div>
+		@endcan
+		<div class="tools">
+			<a class="collapse" href="javascript:;"></a>
+			<a class="config" data-toggle="modal" href="#grid-config"></a>
+			<a class="reload" href="javascript:;"></a>
+			<a class="remove" href="javascript:;"></a>
+		</div>
+		<div class="col-sm-12 my-3">
+			<div class="table-responsive">
+				<table class="table" data-datatable style="width:100%">
+					<thead>
+						<tr>
+							<th scope="col">ID</th>
+							<th scope="col">Naam</th>
+							<th scope="col">Slug</th>
+							<th scope="col">Categorie</th>
+							<th scope="col">Actions</th>
+						</tr>
+					</thead>
+					<tbody>
+						@foreach($products as $product)
+							<tr class="product_line" data-id="{{ $product->id }}">
+								<td>{{ $product->id }}</td>
+						    	<td class="semi-bold">{{ $product->json['name'][ChuckSite::getFeaturedLocale()] }}</td>
+						    	<td>{{$product->slug}}</td>
+						    	<td>{{config('chuckcms-module-order-form.categories')[$product->json['category']]['name']}}</td>
+						    	<td class="semi-bold">
+						    		<a href="{{ route('dashboard.module.order_form.products.edit', ['product' => $product->id]) }}" class="btn btn-primary btn-sm btn-rounded m-r-20">
+						    			<i class="fa fa-pencil"></i> edit
+						    		</a>
+						    		<a href="#" class="btn btn-danger btn-sm btn-rounded m-r-20 product_delete" data-id="{{ $product->id }}">
+						    			<i class="fa fa-trash"></i> delete
+						    		</a>
+						    	</td>
+							</tr>
+						@endforeach
+					</tbody>
+				</table>
+			</div>
+		</div>
+	</div>
+</div>
 @endsection
 
 @section('css')
@@ -76,59 +136,4 @@
     });
 
     </script>
-@endsection
-
-@section('content')
-<div class=" container-fluid   container-fixed-lg">
-    <div class="row">
-		<div class="col-lg-12">
-		<!-- START card -->
-			<div class="card card-transparent">
-				<div class="card-header ">
-					<div class="card-title">Producten</div>
-					<div class="tools">
-						<a class="collapse" href="javascript:;"></a>
-						<a class="config" data-toggle="modal" href="#grid-config"></a>
-						<a class="reload" href="javascript:;"></a>
-						<a class="remove" href="javascript:;"></a>
-					</div>
-				</div>
-				<div class="card-block">
-					<div class="table-responsive">
-						<table class="table table-hover table-condensed" id="condensedTable">
-						<thead>
-							<tr>
-								<th style="width:7%">ID</th>
-								<th style="width:23%">Naam</th>
-								<th style="width:20%">Slug</th>
-								<th style="width:20%">Categorie</th>
-								<th style="width:30%">Actions</th>
-							</tr>
-						</thead>
-							<tbody>
-								@foreach($products as $product)
-								<tr class="product_line" data-id="{{ $product->id }}">
-									<td class="v-align-middle">{{ $product->id }}</td>
-							    	<td class="v-align-middle semi-bold">{{ $product->json['name'][ChuckSite::getFeaturedLocale()] }}</td>
-							    	<td class="v-align-middle">{{$product->slug}}</td>
-							    	<td class="v-align-middle">{{config('chuckcms-module-order-form.categories')[$product->json['category']]['name']}}</td>
-							    	<td class="v-align-middle semi-bold">
-							    		<a href="{{ route('dashboard.module.order_form.products.edit', ['product' => $product->id]) }}" class="btn btn-primary btn-sm btn-rounded m-r-20">
-							    			<i data-feather="edit-2"></i> edit
-							    		</a>
-							    		<a href="#" class="btn btn-danger btn-sm btn-rounded m-r-20 product_delete" data-id="{{ $product->id }}">
-							    			<i data-feather="trash"></i> delete
-							    		</a>
-							    	</td>
-							  	</tr>
-							  	@endforeach
-							</tbody>
-						</table>
-					</div>
-				</div>
-			</div>
-		<!-- END card -->
-		</div>
-    </div>
-</div>
 @endsection
