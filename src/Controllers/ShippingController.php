d5dbeb04956e056f7be837c36e565ef35bb735a1
Karel Brijs <karel@chuck.be>
2020-11-23 11:08:36 +0000

    update address eligibilty

--- src/Controllers/ShippingController.php
+++ src/Controllers/ShippingController.php
@@ -29,14 +29,14 @@ class ShippingController extends Controller
         $limited_to = config('chuckcms-module-order-form.locations.'.$locationKey.'.delivery_limited_to');
 
         if($limited_to == null) {
-            return true;
+            return response()->json(['status' => 'success']);
         } elseif ($limited_to == 'radius') {
             return $this->checkRadius($request);
         } elseif ($limited_to == 'postalcode') {
             return $this->checkPostalcode($request);
         }
 
-        return false;
+        return response()->json(['status' => 'error']);
     }
 
     public function checkRadius(Request $request)
