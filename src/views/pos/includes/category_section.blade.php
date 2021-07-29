9de45e81ea75b8449c889345ba3fb732dc613fdf
Karel Brijs <karel@chuck.be>
2021-07-29 08:41:44 +0000

    update category displayed in pos

--- src/views/pos/includes/category_section.blade.php
+++ src/views/pos/includes/category_section.blade.php
@@ -4,7 +4,7 @@
             <ul class="nav nav-pills" id="navigationTab" role="tablist">
                 @php $c = 0; @endphp
                 @foreach($categories as $category)
-                @if($category->is_displayed)
+                @if($category->is_pos_available)
                 <li class="nav-item mr-2 mr-md-3 mb-1">
                     <a class="nav-link{{ $c == 0 ? ' active' : '' }} px-2 py-1" id="navigationCategory{{$category->id}}Tab" href="#category{{$category->id}}Tab" role="tab" data-toggle="tab" aria-controls="category{{$category->id}}Tab" aria-selected="true"><small>{{ $category->name }}</small></a>
                 </li>
