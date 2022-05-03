80a25035ccd17cefe028f1e0bd39c23036c85177
Karel Brijs <karel@chuck.be>
2022-05-03 17:33:40 +0200

    rmv log statement

--- src/views/frontend/scripts.blade.php
+++ src/views/frontend/scripts.blade.php
@@ -214,8 +214,6 @@ $(document).ready(function() {
 		product_name = $('.chuck_ofm_product_tile[data-product-id='+product_id+']').attr('data-product-name');
 		total_price = parseFloat(current_price) * parseInt(quantity);
 
-		console.log('cof_btnAddProductToCart :: ', product_id, current_price, quantity, product_name, total_price);
-
 		cart_count = parseInt($('#cof_cartTotalQuanity').attr('data-cof-quantity'));
 		if(cart_count == 0) {
 			$('#cof_emptyCartNotice').hide();
